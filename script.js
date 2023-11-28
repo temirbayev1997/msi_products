9e783c81e3f40b92be58c8430070f93be6a384ed
temirbayev1997 <abylaihant2@gmail.com>
2023-11-28 15:52:18 +0600

    msi products i3 - i9

--- script.js
+++ script.js
@@ -1,16 +1,63 @@
 const arr = [
     {id:1,
-    name:'MSI KATANA ',
+    name:'MSI KATANA 15 B12VGK',
     price:0,
     old_price:0,
     image:img,
     characteristic:{
         processer:'intel',
-        processer_model:'',
-        ram:'',
-        ssd:'',
-        display:'',
-        display_hz:'',
-        video_card:''
-    }
-}]
+        processer_model:'i7-12650H',
+        ram: 32,
+        ssd: 1024,
+        display: 15.6,
+        display_hz:'144 hz',
+        video_card:'RTX 4070'
+        }
+    },
+    {id:1,
+        name:'MSI KATANA 15 B13VFK',
+        price:0,
+        old_price:0,
+        image:img,
+        characteristic:{
+            processer:'intel',
+            processer_model:'I7-13620H',
+            ram: 16,
+            ssd: 1024,
+            display: 15.6,
+            display_hz:'144 hz',
+            video_card:'RTX 4060'
+            }
+        },
+    {id:1,
+        name:'MSI KATANA Vector GP77 13VG',
+        price:0,
+        old_price:0,
+        image:img,
+        characteristic:{
+            processer:'intel',
+            processer_model:'I7-13700H',
+            ram: 32,
+            ssd: 1024,
+            display: 17.3,
+            display_hz:'144 hz',
+            video_card:'RTX 4070'
+            }
+        },
+    {id:1,
+        name:'MSI KATANA 15 B13VFK',
+        price:0,
+        old_price:0,
+        image:img,
+        characteristic:{
+            processer:'intel',
+            processer_model:'I7-13620H',
+            ram:'16 Gb',
+            ssd:'1024 Gb',
+            display:'15.6 "',
+            display_hz:'144 hz',
+            video_card:'RTX 4060'
+            }
+        },
+
+]
