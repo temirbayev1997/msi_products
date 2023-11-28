1d2380b03e24acde488406c149eb8c4861adbef2
temirbayev1997 <abylaihant2@gmail.com>
2023-11-28 14:50:18 +0600

    firts commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const arr = [
+    {id:1,
+    name:'MSI KATANA ',
+    price:0,
+    old_price:0,
+    image:img,
+    characteristic:{
+        processer:'intel',
+        processer_model:'',
+        ram:'',
+        ssd:'',
+        display:'',
+        display_hz:'',
+        video_card:''
+    }
+}]
